8f3d1c918fabcd0a356a966e6dbfa0d54389322d
Mustafaa Ahmad <mustafaaahmad@mustafaas-mbp.home>
2019-01-23 18:48:31 +0000

    Chapter 4 completed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -28,7 +28,18 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 # Application definition
+
+STATICFILES_DIRS =[STATIC_DIR, ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -55,7 +66,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
